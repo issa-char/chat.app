6f19ee2c63b78efb01738bd6526fadbbacc5edd3
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-13 05:39:30 -0500

    integrating socket.io

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
 		res.sendFile(new URL('./index.html', import.meta.url).pathname);
+});
+
+io.on('connection', (socket) => {
+		console.log('a user connected');
+		socket.on('disconnect', () => {
+				console.log('user disconnected');
+		});
 });
 
 server.listen(3000, () => {
